8d79a79e2fa4bc940e8840b569608535d6cc5758
unknown <antoinelecorre56@gmail.com>
2023-11-26 00:01:06 +0100

    aie

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -1,8 +1,8 @@
-varying vec4 vModelPosition;
+varying float vModelPosition;
 
 void main(){
     vec4 color;
-    if(vModelPosition.y > 0.0){
+    if(vModelPosition > 0.0){
         color = vec4(1.0,1.0,1.0,1.0);
     }else{
         color = vec4(1.0,1.0,0.0,1.0);
--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -1,10 +1,10 @@
-varying vec4 vModelPosition;
+varying float vModelPosition;
 
 uniform float uTime;
 
 void main(){
     vec4 modelPosition = modelMatrix * vec4(position,1.0);
-    vModelPosition = modelPosition + sin(uTime)*0.05;
+    vModelPosition = modelPosition.y + sin(uTime*10.0)*0.05 + modelPosition.x;
     vec4 viewPositon = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPositon;
 

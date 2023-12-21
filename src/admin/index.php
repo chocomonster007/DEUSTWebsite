4e95a5f5c7c3e2fa44904ae0c0b1c0d201228f96
antoine lecorre <antoinelecorre56@gmail.com>
2023-12-21 09:10:32 +0100

    bbbbb

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,12 +1,5 @@
 <?php
-session_start();
 $pdo = new PDO("mysql:host=localhost;dbname=jeu", "root", "root");
-
-$pdo->exec("INSERT INTO ajout SET reponse1='Utiliser ton ordi', reponse2='Utiliser l\'ordi du deust'");
-$pdo->exec("INSERT INTO ajout SET reponse1='Aller en cours en vélo sous la pluie', reponse2='Prendre un bus bondé de monde'");
-
-$pass = password_hash('Lereste@:11.04',PASSWORD_BCRYPT);
-$pdo->exec("INSERT INTO account SET username = 'KingOfDeust', password = '$pass'");
 
 
 ?>
